65c30fd0b64196acee5c57d20accd2d8b98a1943
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-02-13 12:19:27 -0200

    Começando o aprenziado com uma interface basica

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author felipe.090999
+ */
+public class Calculadora {
+    int Soma(int a, int b){
+        return a + b;
+    }
+    
+    int Sub(int a, int b){
+        return a - b;
+    }
+    
+    int Multi(int a, int b){
+        return a * b;
+    }
+}
