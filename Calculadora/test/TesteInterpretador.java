03b2506d952a3d3e7b2d0e1270577ed2bc6bbd03
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2019-03-10 22:21:32 -0300

    Implemetado testador da classe interpretador

---
+++ Calculadora/test/TesteInterpretador.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Felpi
+ */
+public class TesteInterpretador {
+    
+    Interpretador inter;
+    
+    
+    public TesteInterpretador() {
+        inter = new Interpretador();        
+    }   
+    
+    @Test
+    public void TesteSepararOperacoes()
+    {
+        String expressao = "13-233+54-34+434*4/2";
+        
+        ArrayList<String> ResultEsperado = new ArrayList(); 
+        ResultEsperado.add("-");
+        ResultEsperado.add("+");
+        ResultEsperado.add("-");
+        ResultEsperado.add("+");
+        ResultEsperado.add("*");
+        ResultEsperado.add("/");
+        
+        ArrayList<String> ResultEncontrado = new ArrayList();
+        ResultEncontrado = inter.SepararOperacoes(expressao);
+        
+        assertEquals(ResultEsperado, ResultEncontrado);       
+    }
+    
+    @Test
+    public void TesteSepararNumeros()
+    {
+        String expressao = "13-233+54-34+434*4/2";
+        
+        ArrayList<String> ResultEsperado = new ArrayList(); 
+        ResultEsperado.add("13");
+        ResultEsperado.add("233");
+        ResultEsperado.add("54");
+        ResultEsperado.add("34");
+        ResultEsperado.add("434");
+        ResultEsperado.add("4");
+        ResultEsperado.add("2");
+        
+        ArrayList<String> ResultEncontrado = new ArrayList();
+        ResultEncontrado = inter.SepararNumeros(expressao);
+        
+        assertEquals(ResultEsperado, ResultEncontrado);       
+    }
+    
+    @Test
+    public void TesteContar()
+    {
+        String expressao = "13-233+54-34+434*4/2";
+        
+        String ResultEsperado = "negative668";
+        
+        
+        while(expressao.contains("-"))
+        {
+            expressao = inter.Contar(expressao);
+        }
+        
+        String ResultEncontrado= expressao;
+        
+        assertEquals(ResultEsperado, ResultEncontrado); 
+        
+    }
+    
+    
+}
